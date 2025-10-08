8fdfead0aa784f6ac4ea911dfebaa1390bf191be
fsmendes04 <fso.mendes@campus.fct.unl.pt>
2025-10-08 12:27:15 +0100

    Set Up and Register Account

--- code/backend/src/config/supabaseClient.ts
+++ code/backend/src/config/supabaseClient.ts
@@ -9,6 +9,7 @@ export class SupabaseService {
   constructor(private configService: ConfigService) {
     const supabaseUrl = this.configService.get<string>('SUPABASE_URL');
     const supabaseKey = this.configService.get<string>('SUPABASE_SERVICE_ROLE_KEY');
+
     if (!supabaseUrl || !supabaseKey) {
       throw new Error('Supabase URL and Key must be provided');
     }
